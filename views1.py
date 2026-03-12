2efe7b68b377e0ff977b7d034073b0c228991251
malladi5hitesh-jpg <malladi5hitesh@gmail.com>
2026-03-13 01:57:19 +0530

    Update views1.py

--- views1.py
+++ views1.py
@@ -1,6 +1,14 @@
 import logging
 logger = logging.getLogger(__name__)
-if username == "admin" and password == "pass":
-    logger.info("admin logged in successfully")
-else:
-    logger.warning("Invalid login attempt")
+logging.basicConfig(level=logging.INFO)
+@app.route('/login', methods=['GET','POST'])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if username == "admin" and password == "pass":
+        logger.info("admin logged in successfully")
+        return redirect(url_for('index'))
+    else:
+        logger.warning("Invalid login attempt")
+        return render_template("login.html")
